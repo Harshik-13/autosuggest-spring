48cec5bf5491d02824f9076ddfc07809bf5eaacf
Harshik-13 <jagirapuharshik13@gmail.com>
2026-07-17 15:13:32 +0530

    DOCKERFILE

---
+++ src/test/java/com/HK/Auto_suggest/AutoSuggestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.HK.Auto_suggest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoSuggestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
